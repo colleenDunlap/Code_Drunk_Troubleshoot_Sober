7eadb825ce927fdf47ab98d39704276d40ddf8f5
Dunlap, Colleen <colleen.dunlap@marquette.edu>
2019-02-16 19:44:33 -0600

    writing buddy project initialized

---
+++ writing_buddy/main.py
@@ -0,0 +1,16 @@
+import PyPDF2
+
+#pdf import code from https://www.geeksforgeeks.org/working-with-pdf-files-in-python/
+
+#file Object
+pdfFileObj = open('samplePDFMultiPage.pdf', 'rb')
+#reader Object
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+print(pdfReader.numPages)
+numPages = pdfReader.numPages
+#page Object
+for iPage in range(0,numPages):
+	pageObj = pdfReader.getPage(iPage)
+	#print(pageObj.extractText())
+#extract Text from Page
+pdfFileObj.close()
